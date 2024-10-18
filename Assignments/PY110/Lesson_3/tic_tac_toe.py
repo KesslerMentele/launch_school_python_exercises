c9725457478d7c1e3b9f812522a8102c13b60752
Kessler Mentele <kesslermentele@gmail.com>
2024-10-18 15:30:35 -0700

    PY110 Lesson 3 Tic Tac Toe Completed

--- Assignments/PY110/Lesson_3/tic_tac_toe.py
+++ Assignments/PY110/Lesson_3/tic_tac_toe.py
@@ -1,4 +1,4 @@
-'''
+"""
 
 1. Start
     - Choose Difficulty (?)
@@ -24,12 +24,17 @@
 9. if yes goto 1
 10. end
 
-'''
+"""
 import random
+import os
+from copy import deepcopy
 
 DOUBLE_BAR = '| \u2588   \u2588 '
 SINGLE_BAR = '|   \u2588   '
 EMPTY = '|       '
+NUMBER = '|   {}   '
+SPACER = '\n+-------+-------+-------+\n'
+WIN_LINE = '\033[32m\u2588\033[0m'
 
 def initialize_board()->list:
     return [
@@ -44,8 +49,8 @@ def initialize_board()->list:
     '9']
 ]
 
-
-def send(message:str)->None:
+#  I/O
+def say(message:str)->None:
     print(f'==> {message}')
 
 
@@ -58,89 +63,289 @@ def is_valid_choice(bs:list, val:int)->bool:
 
 
 def get_square(bs:list)->list:
-    new_state = bs.copy()
+    new_state = deepcopy(bs)
+    options = get_options(bs)
+    options = [val + 1 for val in options] # move out of index to count
     while True:
         try:
-            val = int(ask('Choose a Square! ')) - 1 # Put in index terms
+            say(f'Choose a Square! ({join_or(options, ", ", "or")})')
+            val = int(ask(' ')) - 1 # Put in index terms
             if 9 > val >= 0 and is_valid_choice(bs, val):
                 new_state[val // 3][val % 3] = 'X'
                 return new_state
             else:
-                send('Please Choose a Valid Number 1-9')
+                say('Please Choose a Valid Number: ' + join_or(options,', ', 'or'))
         except ValueError:
-            send('Please Choose a Valid Number 1-9')
+            say('Please Choose a Valid Number' + join_or(options,', ', 'or'))
 
 
-def easy_bot(bs:list)->list:
-    new_state = bs.copy()
-    options = [index for index, _
-               in enumerate([item for sublist in bs for item in sublist])
-               if is_valid_choice(bs, index)]
+def print_board(bs:list)->None:
+    print(''.join([row for sub_list in make_board(bs)
+                   for row in sub_list]))
 
-    choice = random.choice(options)
-    new_state[choice // 3][choice % 3] = 'O'
+
+def print_with_win_line(bs:list, line_location:dict)->None:
+    os.system('clear')
+    print(''.join([row for sub_list in add_win_line(bs, line_location)
+                   for row in sub_list]))
+
+
+# Bot(s)
+def easy(bs:list)->list:
+    new_state = deepcopy(bs)
+    options = get_options(bs)
+
+    option = random.choice(options)
+    new_state[option // 3][option % 3] = 'O'
     return new_state
 
 
-def is_over(bs:list):
-    pass
+def necessary_move(bs, options):
+    for option in options:
+        my_play = deepcopy(bs)
+        my_play[option // 3][option % 3] = 'O'
+        has_won = find_winner(my_play)
+        if has_won['win'] and has_won['winner'] == 'O':
+            return my_play
+        their_play = deepcopy(bs)
+        their_play[option // 3][option % 3] = 'X'
+        has_won = find_winner(their_play)
+        if has_won['win'] and has_won['winner'] == 'X':
+            return my_play
+    return None
 
 
-def make_board(bs: list)->str:
-    fancy_board = ''
+def medium(bs:list)->list:
+    options = get_options(bs)
+    need = necessary_move(bs, options)
+    new_state = deepcopy(bs)
+    if need:
+        return need
+    return easy_bot(bs)
+
+
+def hard(bs:list)->list:
+    options = get_options(bs)
+    need = necessary_move(bs, options)
+    new_state = deepcopy(bs)
+    if need:
+        return need
+    if 4 in options:
+        new_state[1][1] = 'O'
+        return new_state
+    return easy_bot(bs)
+
+
+# Board Construction
+def make_board(bs: list)->list:
+    fancy_board = []
+
+    def make_row(number, r: list) -> list:
+        fancy_row_full = []
+        # Loop for each row of the board
+        for line in range(1, 4):
+            fancy_row_line = ''
+            # Loop for each text row (each row is three lines tall)
+            for inner_index, item in enumerate(r):
+                if item == 'X':
+                    fancy_row_line += SINGLE_BAR if line % 2 == 0 \
+                        else DOUBLE_BAR
+
+                elif item == 'O':
+                    fancy_row_line += DOUBLE_BAR if line % 2 == 0 \
+                        else SINGLE_BAR
+
+                else:
+                    if line % 2 == 0:
+                        fancy_row_line += NUMBER.format(number * len(r) + inner_index + 1)
+                    else:
+                        fancy_row_line += EMPTY
+
+            fancy_row_line += '|\n' if line % 3 else '|'
+            fancy_row_full.append(fancy_row_line)
+
+        return fancy_row_full
+
     for index, row in enumerate(bs):
-        fancy_board += make_row(index, row)
+        fancy_board.append(make_row(index, row))
         if index + 1 != len(bs):
-            fancy_board += '\n+-------+-------+-------+\n'
+            fancy_board[index].append(SPACER)
     return fancy_board
 
 
-def make_row(number, row:list)->str:
-    fancy_row = ''
-    for line in range(1,4):
-        for index, item in enumerate(row):
-            if item == 'X':
-                fancy_row += SINGLE_BAR if line % 2 == 0 \
-                    else DOUBLE_BAR
+def add_win_line(bs:list, line_location:dict)->list:
+    board_list = make_board(bs)
 
-            elif item == 'O':
-                fancy_row += DOUBLE_BAR if line % 2 == 0 \
-                    else SINGLE_BAR
+    match line_location['type']:
+        case 'horizontal':
+            line_to_change = board_list[line_location['line']][1]
+            board_list[line_location['line']][1] =  (
+                ''.join([WIN_LINE if char in ' \u2588' else char for char in line_to_change]))
+        case 'vertical':
+            for row in range(3):
+                for index,line in enumerate(board_list[row]):
+                    if index != 3:
+                        match line_location['line']:
+                            case 0:
+                                board_list[row][index] = (line[:4] + WIN_LINE +
+                                                          line[5:])
+                            case 1:
+                                board_list[row][index] = (line[:12] + WIN_LINE +
+                                                          line[13:])
+                            case 2:
+                                board_list[row][index] = (line[:20] + WIN_LINE +
+                                                          line[21:])
+        case 'diagonal':
+            chars_placed = 1
 
-            else:
-                if line % 2 == 0:
-                    fancy_row += f'|   {number * len(row) + index + 1}   '
-                else:
-                    fancy_row += EMPTY
+            if line_location['line'] == 0:
+                for row in range(3):  # Iterate over rows
+                    for index, line in enumerate(board_list[row]):
+                        if index != 3:
+                            replace_pos = chars_placed + 1
+                            board_list[row][index] = line[:replace_pos] + WIN_LINE + line[replace_pos + 1:]
 
-        fancy_row += '|\n' if line % 3 else '|'
+                        chars_placed += 2
+            elif line_location['line'] == 1:
+                chars_placed = 2
 
-    return fancy_row
+                for row in range(3):
+                    for index, line in enumerate(board_list[row]):
+                        if index < 2:
+                            # For this diagonal, positions go backward across rows
+                            replace_pos = len(line) - chars_placed - 2
+                            board_list[row][index] = line[:replace_pos] + WIN_LINE + line[replace_pos + 1:]
+                        elif index == 2:
+                            replace_pos = len(line) - chars_placed - 1
+                            board_list[row][index] = line[:replace_pos] + WIN_LINE + line[replace_pos + 1:]
+                        chars_placed += 2
+
+    return board_list
 
 
-def find_winner(bs:list)->tuple:
+# Thinking
+def find_winner(bs:list)->dict:
     to_check = ['X', 'O']
+
     for check in to_check:
         # find row
-        if all([item == check for item in bs[0]]):
-            send('X WINS!')
-            return True, check
-        # find column
-        
+        for index in range(3):
+            if all([item == check for item in bs[index]]):
+                return {'win':True,'winner': check, 'line_location':{'type':'horizontal', 'line':index, 'winner':check}}
+            # find column
+            elif all([sub_list[index] == check for sub_list in bs]):
+                return {'win':True,'winner': check, 'line_location': {'type': 'vertical', 'line': index, 'winner':check}}
+        # diagonals
+        if bs[0][0] == bs[1][1] == bs[2][2] == check:
+            return {'win':True,'winner': check, 'line_location': {'type': 'diagonal', 'line': 0, 'winner':check}}
+        elif bs[0][2] == bs[1][1] == bs[2][0] == check:
+            return {'win':True,'winner': check, 'line_location': {'type': 'diagonal', 'line': 1, 'winner':check}}
+    if all([item in 'XO' for sublist in bs for item in sublist]):
+        return {'win':True,'winner': None}
+    return {'win':False, 'winner':None}
 
 
+def get_options(bs:list)->list:
+    return [index for index, _
+               in enumerate([item for sublist in bs for item in sublist])
+               if is_valid_choice(bs, index)]
+
+
+def join_or(values:list, sep_one:str = ', ', sep_two:str = 'or')->str:
+    count = len(values)
+    match count:
+        case 0:
+            return ''
+        case 1:
+            return str(values[0])
+        case 2:
+            return ' '.join([str(values[0]), sep_two, str(values[1])])
+        case _ if count > 2 :
+               return sep_one.join(str(val) if idx != len(values) - 1
+                                    else ' '.join([sep_two, str(val)])
+                                    for idx, val in enumerate(values))
 
 
 def run_game():
-    board_state = initialize_board()
-    print(make_board(board_state))
-    winner = False
-    while not winner:
-        board_state = get_square(board_state)
-        board_state = easy_bot(board_state)
-        print(make_board(board_state))
-        winner = find_winner(board_state)
-    if ask('Again?'):
-        run_game()
+    running = True
+    game_count = 1
+    score = [0,0]
+    winner = None
+    bots = [easy, medium, hard]
+
+
+    def set_difficulty():
+        say('Please choose a difficulty:')
+        bot = ask('1: Easy \n==> 2: Medium \n==> 3: Hard\n')
+        while bot not in ['1', '2', '3']:
+            say(f'Please choose either: {join_or([1, 2, 3], ", ", "or")}')
+            bot = ask('')
+        return bots[int(bot) - 1]
+
+
+    os.system('clear')
+    say('Welcome to Tic-Tac-Toe!')
+    bot = set_difficulty()
+
+    while running:
+        has_won = {'win':False, 'winner':None}
+
+        board_state = initialize_board()
+
+        while not has_won['win']:
+            os.system('clear')
+            print(f"Difficulty: {bot.__name__}")
+            print(f'Round: {str(game_count)}')
+            print(f'Score: {score[0]} to {score[1]} (best of 5)')
+            print_board(board_state)
+            for turn in [get_square, bot]:
+                board_state = turn(board_state)
+                has_won = find_winner(board_state)
+                if has_won['win']:
+                    if has_won['winner'] == 'X':
+                        os.system('clear')
+                        print_with_win_line(board_state, has_won['line_location'])
+                        say('You Win the Round!')
+                        score[0] += 1
+                        break
+                    elif has_won['winner'] == 'O':
+                        os.system('clear')
+                        print_with_win_line(board_state, has_won['line_location'])
+                        say('You Lose the Round!')
+                        score[1] += 1
+                        break
+                    else:
+                        os.system('clear')
+                        print_board(board_state)
+                        say('It\'s a Tie!')
+                        break
+                else:
+                    print_board(board_state)
+        game_count +=1
+        if score[0] > 2:
+            game_count = 1
+            score = [0, 0]
+            winner = 'X'
+            say('YOU WIN THE MATCH!')
+        elif score[1] > 2:
+            game_count = 1
+            score = [0, 0]
+            winner = 'O'
+            say('YOU LOSE THE MATCH!')
+
+
+        answer = ask(f"Next {'Game' if winner else 'Round' }? (Y/N)")
+        while answer.lower() not in ['y','n']:
+            say('Please Choose Y or N')
+            answer = ask(f'Play Again? (Y/N) {"or Change the Difficulty? (D)" if winner else ""}')
+
+        if answer.lower() == 'n':
+            running = False
+            os.system('clear')
+            say('Thanks For Playing!')
+        elif answer.lower() == 'd' and winner:
+            bot = set_difficulty()
+
+
 
 run_game()
