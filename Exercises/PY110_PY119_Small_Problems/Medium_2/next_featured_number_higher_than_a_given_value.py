535c0e8e555fa0fc8e2861602c530d5bd3576dc7
Kessler Mentele <kesslermentele@gmail.com>
2024-10-23 11:00:32 -0700

    PY110 Lesson 3 Medium 2 work

---
+++ Exercises/PY110_PY119_Small_Problems/Medium_2/next_featured_number_higher_than_a_given_value.py
@@ -0,0 +1,29 @@
+"""
+a featured number:
+- Odd
+- Multiple of 7
+- with digits 0-9 occurring at most once each
+
+Input: an integer
+Output: integer equaling the next featured number larger than the given integer
+
+
+"""
+
+def next_featured(floor:int)->int | str:
+    pass
+
+
+print(next_featured(12) == 21)                  # True
+print(next_featured(20) == 21)                  # True
+print(next_featured(21) == 35)                  # True
+print(next_featured(997) == 1029)               # True
+print(next_featured(1029) == 1043)              # True
+print(next_featured(999999) == 1023547)         # True
+print(next_featured(999999987) == 1023456987)   # True
+print(next_featured(9876543186) == 9876543201)  # True
+print(next_featured(9876543200) == 9876543201)  # True
+
+error = ("There is no possible number that "
+         "fulfills those requirements.")
+print(next_featured(9876543201) == error)       # True
